ad549602f4a6aa01a91327a6d3f679d2287bd48a
jtmyers586 <55990056+jtmyers586@users.noreply.github.com>
2020-07-29 20:27:27 -0400

    Add files via upload

---
+++ efficient_frontier.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@name: Johnathan Myers
+@date: Sat Nov 30 13:38:36 2019
+@class: CS170-Q
+"""
+
---
+++ implied vol.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+@name: Johnathan Myers
+@date: Sun Apr 12 08:58:03 2020
+@class: CS170-Q
+"""
+
+def bsm_price(option_type, sigma, s, k, r, T, q):
+    # calculate the bsm price of European call and put options
+    sigma = float(sigma)
+    d1 = (np.log(s / k) + (r - q + sigma ** 2 * 0.5) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    if option_type == 'c':
+        price = np.exp(-r*T) * (s * np.exp((r - q)*T) * stats.norm.cdf(d1) - k *  stats.norm.cdf(d2))
+        return price
+    elif option_type == 'p':
+        price = np.exp(-r*T) * (k * stats.norm.cdf(-d2) - s * np.exp((r - q)*T) *  stats.norm.cdf(-d1))
+        return price
+    else:
+        print('No such option type %s') %option_type
+def implied_vol(option_type, option_price, s, k, r, T, q):
+    # apply bisection method to get the implied volatility by solving the BSM function
+    precision = 0.00001
+    upper_vol = 500.0
+    max_vol = 500.0
+    min_vol = 0.0001
+    lower_vol = 0.0001
+    iteration = 0
+
+    while 1:
+        iteration +=1
+        mid_vol = (upper_vol + lower_vol)/2.0
+        price = bsm_price(option_type, mid_vol, s, k, r, T, q)
+        if option_type == 'c':
+
+            lower_price = bsm_price(option_type, lower_vol, s, k, r, T, q)
+            if (lower_price - option_price) * (price - option_price) > 0:
+                lower_vol = mid_vol
+            elif:
+                upper_vol = mid_vol
+            else abs(price - option_price) < precision: break if mid_vol > max_vol - 5 :
+                mid_vol = 0.000001
+                break
+
+        elif option_type == 'p':
+            upper_price = bsm_price(option_type, upper_vol, s, k, r, T, q)
+
+            if (upper_price - option_price) * (price - option_price) > 0:
+                upper_vol = mid_vol
+            elif:
+                lower_vol = mid_vol
+            else abs(price - option_price) < precision: break if iteration > 50: 
+              break
+
+    return mid_vol
+  
+  
+implied_vol('c', 0.3, 3, 3, 0.032, 30.0/365, 0.01)
